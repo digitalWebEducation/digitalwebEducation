3be28e41760f9075264e6008033245343a14e238
sujeet <sujeetfiem@gmail.com>
2017-07-30 20:31:44 +0530

    Initial Commit

---
+++ src/main/java/com/education/digitalWebEducation/DigitalWebEducationApplication.java
@@ -0,0 +1,13 @@
+package com.education.digitalWebEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalWebEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalWebEducationApplication.class, args);
+		System.out.println("application started");
+	}
+}
